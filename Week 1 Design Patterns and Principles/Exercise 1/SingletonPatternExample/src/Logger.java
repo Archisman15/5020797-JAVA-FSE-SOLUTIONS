44ca41ce2bc03f510e848485af3f6ac535fe2c6c
Archisman Bhowmick <107766606+Archisman15@users.noreply.github.com>
2024-07-31 08:57:46 +0530

    Update Logger.java

--- Week 1 Design Patterns and Principles/Exercise 1/SingletonPatternExample/src/Logger.java
+++ Week 1 Design Patterns and Principles/Exercise 1/SingletonPatternExample/src/Logger.java
@@ -1,13 +1,9 @@
 public class Logger {
-    // Private static instance of the Logger class
     private static Logger instance;
-
-    // Private constructor to prevent instantiation
+    
     private Logger() {
-        // Initialize any resources if needed
     }
 
-    // Public method to provide access to the instance
     public static Logger getInstance() {
         if (instance == null) {
             instance = new Logger();
@@ -15,7 +11,6 @@ public class Logger {
         return instance;
     }
 
-    // Example logging method
     public void log(String message) {
         System.out.println("Log: " + message);
     }
